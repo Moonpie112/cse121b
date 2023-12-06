b2dc4938d8870aaaf408b29c0942f08ff85ae242
Moonpie112 <kaylanonemoon@gmail.com>
2023-12-06 14:31:12 -0700

    changes to W5

--- scripts/w05-task.js
+++ scripts/w05-task.js
@@ -15,14 +15,19 @@ const displayTemples = (temples) => {
         // creates the h3 element
         const h3Element = document.createElement('h3');
         // adds the temple's name to this element
-        h3Element.textContent
-        
+        h3Element.textContent = temple.templeName
 
         // creates the img element
-        // const imgElement = document.createElement('img');
+        const imgElement = document.createElement('img');
         // adds temple URL to imageUrl to the src
-
+        imgElement.setAttribute("src", temple.imageUrl) 
         // adds temple's location property to the alt attribute
+        imgElement.setAttribute("alt", temple.location)
+
+        // appends the info to the webpage so we can see the changes made
+        articleElement.appendChild(h3Element)
+        articleElement.appendChild(imgElement)
+        templesElement.appendChild(articleElement)
     });
 };
 
@@ -31,21 +36,43 @@ const displayTemples = (temples) => {
 const getTemples = async () => {
     const response = await fetch("https://byui-cse.github.io/cse121b-ww-course/resources/temples.json");
     const temples = await response.json();
-    console.log(temples);
-    console.log(temples[0].area);
-    console.log(temples[0].templeName);
+    templeList.push(temples);
+    displayTemples(temples);
 }
 
-
-getTemples()
-
 /* reset Function */
-
+const reset = () => {
+    templesElement.textContent = ''; 
+};
 
 /* sortBy Function */
 
+const sortBy = (temples) => {
+    reset();
+    let filter = document.getElementbyID('sortBy');
+    
+    switch (filter) {
+        case utah:
+            displayTemples("temples");
+            const filterUtah = templeList.filter(temple => {return temple.location.includes("Utah");
+            });
+            break;
+        case notutah:
+        displayTemples(temples);
 
+            break;
+        case older:
+            displayTemples(temples);
 
-// getTemples();
+            break;
+        case all:
+            displayTemples(temples);
+
+            break;
+    }
+};
+
+getTemples();
 
 /* Event Listener */
+document.querySelector("#sortBy").addEventListener("change", () => {sortBy(templeList)});
